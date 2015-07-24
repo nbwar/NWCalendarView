96f47a8d8726a0cc91e0b1738eec039dd111ee16
Nicholas Wargnier <nwargnier@gmail.com>
2015-07-23 17:10:08 -0700

    Initial Commit

---
+++ NWCalendarDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NWCalendarDemo
+//
+//  Created by Nicholas Wargnier on 7/23/15.
+//  Copyright (c) 2015 Nick Wargnier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
